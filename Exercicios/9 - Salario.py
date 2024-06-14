75c42a88fe2d63a65a27388e23d3edbcac9c15a7
FabBarbosa <91352941+FabBarbosa@users.noreply.github.com>
2024-06-14 09:06:21 -0300

    Commitando exercicios

---
+++ Exercicios/9 - Salario.py
@@ -0,0 +1,7 @@
+NUMBER = int(input())
+Work_hours = int(input())
+Price_hour = float(input())
+Anual_sallary = Work_hours * Price_hour
+
+print("NUMBER =",NUMBER)
+print("SALARY = U$ %.2f" %Anual_sallary)
